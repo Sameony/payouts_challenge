ffd4c45ebbfb1a3e02e9c9e140cc5824383f5d46
mohammedsameer <mohammed.sameer@nagarro.com>
2023-09-26 17:19:34 +0530

    initial challenge completed

---
+++ tableData.js
@@ -0,0 +1,53 @@
+const pendingData = [
+    {
+        order_id:"#281029",
+        status:'Pending',
+        date:"27 July, 2023",
+        amount:"₹234.69"
+    },{
+        order_id:"#281029",
+        status:'Pending',
+        date:"27 July, 2023",
+        amount:"₹234.69"
+    },{
+        order_id:"#281029",
+        status:'Pending',
+        date:"27 July, 2023",
+        amount:"₹234.69"
+    },{
+        order_id:"#221029",
+        status:'Pending',
+        date:"27 July, 2023",
+        amount:"₹234.69"
+    },{
+        order_id:"#281029",
+        status:'Pending',
+        date:"22 August, 2023",
+        amount:"₹234.69"
+    },{
+        order_id:"#281029",
+        status:'Pending',
+        date:"27 July, 2023",
+        amount:"₹234.69"
+    },{
+        order_id:"#281029",
+        status:'Pending',
+        date:"27 July, 2023",
+        amount:"₹234.69"
+    },{
+        order_id:"#281029",
+        status:'Pending',
+        date:"27 July, 2023",
+        amount:"₹234.69"
+    },{
+        order_id:"#281029",
+        status:'Pending',
+        date:"27 July, 2023",
+        amount:"₹234.69"
+    },{
+        order_id:"#281029",
+        status:'Pending',
+        date:"27 July, 2023",
+        amount:"₹234.69"
+    }
+]
