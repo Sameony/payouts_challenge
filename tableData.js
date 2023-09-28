08e2ea5318dad53cb8779180caf25e2bd1fcbadc
mohammedsameer <mohammed.sameer@nagarro.com>
2023-09-28 16:13:27 +0530

    feat: different data display per user selection

--- tableData.js
+++ tableData.js
@@ -51,3 +51,191 @@ const pendingData = [
         amount:"₹234.69"
     }
 ]
+
+const EligibleData = [
+    {
+        order_id:"#281029",
+        status:'Accepted',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        fees:"₹20",
+        total:"₹254.69"
+    },{
+        order_id:"#281029",
+        status:'Shipped',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        fees:"₹20",
+        total:"₹254.69"
+    },{
+        order_id:"#281029",
+        status:'Accepted',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        fees:"₹20",
+        total:"₹254.69"
+    },{
+        order_id:"#221029",
+        status:'Delivered',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        fees:"₹20",
+        total:"₹254.69"
+    },{
+        order_id:"#281029",
+        status:'Delivered',
+        date:"22 August, 2023",
+        amount:"₹234.69",
+        fees:"₹20",
+        total:"₹254.69"
+    },{
+        order_id:"#281029",
+        status:'Shipped',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        fees:"₹20",
+        total:"₹254.69"
+    },{
+        order_id:"#281029",
+        status:'Delivered',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        fees:"₹20",
+        total:"₹254.69"
+    },{
+        order_id:"#281029",
+        status:'Delivered',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        fees:"₹20",
+        total:"₹254.69"
+    },{
+        order_id:"#281029",
+        status:'Delivered',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        fees:"₹20",
+        total:"₹254.69"
+    },{
+        order_id:"#281029",
+        status:'Delivered',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        fees:"₹20",
+        total:"₹254.69"
+    }
+]
+
+const RefundData =  [
+    {
+        order_id:"#281029",
+        status:'Pending',
+        trans_id:"3201848572",
+        date:"27 July, 2023",
+        amount:"₹234.69",
+    },{
+        order_id:"#281029",
+        status:'Pending',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        trans_id:"3201848572",  
+    },{
+        order_id:"#281029",
+        status:'Processing',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        trans_id:"3201848572",  
+    },{
+        order_id:"#281029",
+        status:'Successful',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        trans_id:"3201848572",  
+    },{
+        order_id:"#281029",
+        status:'Processing',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        trans_id:"3201848572",  
+    },{
+        order_id:"#281029",
+        status:'Successful',
+        date:"27 July, 2023",
+        amount:"₹234.69",
+        trans_id:"3201848572",  
+    }
+]
+
+const PayoutData = [
+    {
+        trans_id:"#281029",
+        status:'Processing',
+        date:"29 July, 2023",
+        order_amount:"₹234.69",
+        fees:"₹1000",
+        total:"₹254.69"
+    },{
+        trans_id:"#281029",
+        status:'Successful',
+        date:"29 July, 2023",
+        order_amount:"₹234.69",
+        fees:"₹1000",
+        total:"₹254.69"
+    },{
+        trans_id:"#281029",
+        status:'Successful',
+        date:"29 July, 2023",
+        order_amount:"₹234.69",
+        fees:"₹1000",
+        total:"₹254.69"
+    },{
+        trans_id:"#221029",
+        status:'Processing',
+        date:"29 July, 2023",
+        order_amount:"₹234.69",
+        fees:"₹1000",
+        total:"₹254.69"
+    },{
+        trans_id:"#281029",
+        status:'Successful',
+        date:"29 August, 2023",
+        order_amount:"₹234.69",
+        fees:"₹1000",
+        total:"₹254.69"
+    },{
+        trans_id:"#281029",
+        status:'Successful',
+        date:"29 July, 2023",
+        order_amount:"₹234.69",
+        fees:"₹1000",
+        total:"₹254.69"
+    },{
+        trans_id:"#281029",
+        status:'Successful',
+        date:"29 July, 2023",
+        order_amount:"₹234.69",
+        fees:"₹1000",
+        total:"₹254.69"
+    },{
+        trans_id:"#281029",
+        status:'Successful',
+        date:"29 July, 2023",
+        order_amount:"₹234.69",
+        fees:"₹1000",
+        total:"₹254.69"
+    },{
+        trans_id:"#281029",
+        status:'Successful',
+        date:"29 July, 2023",
+        order_amount:"₹234.69",
+        fees:"₹1000",
+        total:"₹254.69"
+    },{
+        trans_id:"#281029",
+        status:'Successful',
+        date:"29 July, 2023",
+        order_amount:"₹234.69",
+        fees:"₹1000",
+        total:"₹254.69"
+    }
+]
